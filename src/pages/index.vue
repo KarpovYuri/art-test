2629d862189ed8647463a31f8a92acd15d693c6f
Karpov <yurick@bk.ru>
2024-10-27 00:41:29 +0300

    Add locale select

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,12 @@
 <template>
+	<select
+		v-model="localization"
+		class="w-24 border border-slate-500 outline-none mb-4 rounded text-black text-lg"
+	>
+		<option>ru</option>
+		<option>en</option>
+		<option>fr</option>
+	</select>
 	<ul class="flex flex-col gap-2">
 		<tree-element
 			v-for="element in treeElements"
@@ -10,7 +18,8 @@
 
 <script setup lang="ts">
 import jsonData from '@/utils/task.json'
-import TreeElement from "@/components/TreeElement.vue";
+import TreeElement from '@/components/TreeElement.vue'
 
 const treeElements = jsonData as unknown as Global.TreeElement[]
+const localization = ref('ru')
 </script>
